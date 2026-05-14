44754558b55d81483e536f38cdbee501745caf3f
Harika Pullamsetty <harikapullamsetty@Harikas-MacBook-Air.local>
2026-05-14 14:46:53 +0530

    sittting sticker placement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -243,7 +243,8 @@ function Home() {
             alt="sitting sticker"
             style={{
               position: "absolute",
-              bottom: isMobile ? "390px" : "105px",
+              top: isMobile ? "760px" : undefined,
+              bottom: isMobile ? undefined : "105px",
               right: isMobile ? "38px" : "240px",
               width: isMobile ? "112px" : "145px",
               zIndex: 2,
